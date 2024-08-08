358a39c103ac93b767af9995e35f0487308936ee
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2024-08-08 09:54:35 +0700

    feat: add current time

--- bismillah.py
+++ bismillah.py
@@ -4,6 +4,7 @@ import time
 import json
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()
 driver = webdriver.Chrome()
@@ -22,22 +23,24 @@ with open("matkul.json", "r") as json_file:
 print(matkul_code)
 total_matkul = len(matkul_code)
 
-#Isi ini sesuai akun
+# Isi ini sesuai akun
 user = os.getenv("USER")
 passw = os.getenv("PASSW")
 
-#Nama yang ada di atas kiri homepage (( person MUHAMMAD MADHANI PUTRA – Mahasiswa S1 Reguler Sistem Informasi (06.00.12.01) ))
+# Nama yang ada di atas kiri homepage (( person MUHAMMAD MADHANI PUTRA – Mahasiswa S1 Reguler Sistem Informasi (06.00.12.01) ))
 display_name = os.getenv("DISPLAY_NAME")
 
 chosen_matkul = False
 total_chosen = 0
+
+def get_current_time():
+    return datetime.datetime.now().strftime("%H:%M:%S")
 
 def logout_page():
     driver.get(logout_url)
     time.sleep(0.5)
 
 def login_page():
-
     username = driver.find_element(By.NAME, "u")
     username.clear()
     username.send_keys(user)
@@ -48,7 +51,7 @@ def login_page():
     password.send_keys(passw)
     time.sleep(0.1)
     driver.find_element(By.XPATH, "//input[@value='Login']").click()
-    print("login bangg")
+    print(f"{get_current_time()} - login bangg")
 
 def war_page():
     for name, kode in matkul_code.items():
@@ -56,16 +59,16 @@ def war_page():
             radio_input = driver.find_element(By.XPATH, f"//input[@value='{kode}']")
             global total_chosen
             total_chosen += 1
-            if(not radio_input.is_selected()): 
+            if not radio_input.is_selected(): 
                 radio_input.click()
-                print(f"{name} dipilih! (kode: {kode})")
+                print(f"{get_current_time()} - {name} dipilih! (kode: {kode})")
                 global chosen_matkul
                 chosen_matkul = True
                 time.sleep(0.1)
-            else :
-                print(f"{name} sudah dipilih! (kode: {kode})")
+            else:
+                print(f"{get_current_time()} - {name} sudah dipilih! (kode: {kode})")
         except:
-            print(f"{name} tidak ada! (kode: {kode})")
+            print(f"{get_current_time()} - {name} tidak ada! (kode: {kode})")
             continue
 
     driver.find_element(By.XPATH, "//input[@value='Simpan IRS']").click()
@@ -75,12 +78,9 @@ if __name__ == "__main__":
     driver.get(login_url)
     time.sleep(0.5)
 
-    while(True):
-
-        while(display_name not in driver.page_source and 
-              "Fakultas Kedokteran" not in driver.page_source):
+    while True:
+        while display_name not in driver.page_source and "Fakultas Kedokteran" not in driver.page_source:
             driver.refresh()
-            
 
         if "Fakultas Kedokteran" in driver.page_source:
             login_page()
@@ -93,24 +93,24 @@ if __name__ == "__main__":
 
         if "CoursePlanEdit" in driver.current_url:
             if "Anda tidak dapat mengisi IRS" in driver.page_source:
-                print("blom bisa bang T_T")
+                print(f"{get_current_time()} - blom bisa bang T_T")
                 logout_page()
                 continue
 
-            print("milih dulu bang")
+            print(f"{get_current_time()} - milih dulu bang")
             time.sleep(1.5)
 
-            print("bismillah menang")
+            print(f"{get_current_time()} - bismillah menang")
             
             war_page()
 
-            if (not chosen_matkul and total_chosen < total_matkul):
-                print("ngulang isi T_T")
+            if not chosen_matkul and total_chosen < total_matkul:
+                print(f"{get_current_time()} - ngulang isi T_T")
                 driver.get(siak_url)
                 continue
             
-            print("alhamdulillah")
+            print(f"{get_current_time()} - alhamdulillah")
             break
 
     while True:
-    	pass
+        pass
